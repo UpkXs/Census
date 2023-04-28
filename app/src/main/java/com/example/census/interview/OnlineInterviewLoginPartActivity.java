ec42741d07ee4203b19348e35a34f9ae19c99726
asugurali <asugurali@greet-go.com>
2023-04-28 17:59:38 +0600

    fix():implemented online interview login part logic

--- app/src/main/java/com/example/census/interview/OnlineInterviewLoginPartActivity.java
+++ app/src/main/java/com/example/census/interview/OnlineInterviewLoginPartActivity.java
@@ -3,6 +3,7 @@ package com.example.census.interview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,8 +11,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.census.R;
-import com.example.census.enums.Action;
-import com.example.census.enums.Role;
+import com.example.census.modelInterview.LoginPartDAO;
+import com.example.census.sqliteDatabase.MyDatabaseHelper;
 
 public class OnlineInterviewLoginPartActivity extends AppCompatActivity {
 
@@ -22,6 +23,9 @@ public class OnlineInterviewLoginPartActivity extends AppCompatActivity {
     private EditText editTxtRegion;
 
     private Button btnGoToTheNextPart;
+
+    private MyDatabaseHelper myDB;
+    private LoginPartDAO loginPartDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,27 +39,73 @@ public class OnlineInterviewLoginPartActivity extends AppCompatActivity {
             System.out.println("bkE5lAcB :: username : " + username);
         }
 
+        myDB = new MyDatabaseHelper(OnlineInterviewLoginPartActivity.this);
+
         editTxtTin = findViewById(R.id.tin);
         editTxtFullName = findViewById(R.id.fullName);
         editTxtRegion = findViewById(R.id.region);
 
         btnGoToTheNextPart = findViewById(R.id.btnGoToTheNextPart);
 
+        int citizenUsernameId = getCitizenUsernameId(username);
+
+        if (citizenUsernameId == 0) {
+            System.out.println("BnqxKM1M :: OnlineInterviewLoginPartActivity : " +
+                    "Citizen username ID not found with username : " + username);
+            toastShow("Citizen username ID not found with username : " + username);
+            return;
+        }
+
+        loginPartDAO = getLoginPartDAOInfo(citizenUsernameId);
+
+        if (loginPartDAO.equals(new LoginPartDAO())) {
+            System.out.println("4FfTLAMy :: OnlineInterviewLoginPartActivity : " +
+                    "Citizen username not found with usernameId : " + citizenUsernameId);
+            toastShow("Citizen username not found with usernameId : " + citizenUsernameId);
+            return;
+        }
+
+        editTxtTin.setText(loginPartDAO.getTin());
+        editTxtFullName.setText(loginPartDAO.getFullName());
+        editTxtRegion.setText(loginPartDAO.getRegionName());
+
         btnGoToTheNextPart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                goToTheNextPart(view);
+                goToTheNextPart(view, citizenUsernameId);
             }
         });
 
     }
 
-    private void goToTheNextPart(View view) {
+    private void goToTheNextPart(View view, int citizenUsernameId) {
         if (editTxtTin.getText().toString().trim().isEmpty() ||
                 editTxtFullName.getText().toString().trim().isEmpty() ||
                 editTxtRegion.getText().toString().trim().isEmpty()) {
             System.out.println("cmV84h7M :: OnlineInterviewLoginPartActivity : Some field is not entered. Please enter all the details.");
             toastShow("Some field is not entered. Please enter all the details.");
+            return;
+        }
+
+        String citizenTIN = editTxtTin.getText().toString().trim();
+        String citizenFullName = editTxtFullName.getText().toString().trim();
+        String citizenRegionName = editTxtRegion.getText().toString().trim();
+
+        int citizenRegionId = getRegionId(citizenRegionName);
+
+        if (citizenRegionId == 0) {
+            System.out.println("l115TQzg :: OnlineInterviewLoginPartActivity : " +
+                    "Region ID not found with region name : " + citizenRegionName);
+            toastShow("Region ID not found with region name : " + citizenRegionName);
+            return;
+        }
+
+        long result = myDB.updateCitizen(citizenUsernameId, citizenTIN, citizenFullName, citizenRegionId);
+        if (result == -1) {
+            toastShow("First part of online interview not correctly passed");
+        } else {
+            toastShow("First part of online interview passed successfully");
+            // todo start second part of online interview
         }
     }
 
@@ -65,5 +115,49 @@ public class OnlineInterviewLoginPartActivity extends AppCompatActivity {
 
         Toast toast = Toast.makeText(context, text, duration);
         toast.show();
+    }
+
+    private int getRegionId(String regionName) {
+        Cursor cursor = myDB.selectFromTable("select _id from region where region_name == '" + regionName + "'");
+        int regionId = 0;
+        if (cursor.getCount() == 0) {
+            return regionId;
+        } else {
+            while (cursor.moveToNext()) {
+                regionId = cursor.getInt(0);
+            }
+        }
+        return regionId;
+    }
+
+    private int getCitizenUsernameId(String username) {
+        Cursor cursor = myDB.selectFromTable("select _id from citizen_login where username == '" + username + "'");
+        int usernameId = 0;
+        if (cursor.getCount() == 0) {
+            return usernameId;
+        } else {
+            while (cursor.moveToNext()) {
+                usernameId = cursor.getInt(0);
+            }
+        }
+        return usernameId;
+    }
+
+    private LoginPartDAO getLoginPartDAOInfo(int usernameId) {
+        Cursor cursor = myDB.selectFromTable("select citizen_tin, citizen_fullName, region_name " +
+                "from citizen inner join region " +
+                "where citizen.username_id == " + usernameId +
+                "and citizen.region_id == region._id");
+        LoginPartDAO loginPartDAO = new LoginPartDAO();
+        if (cursor.getCount() == 0) {
+            return loginPartDAO;
+        } else {
+            while (cursor.moveToNext()) {
+                loginPartDAO.setTin(cursor.getInt(0));
+                loginPartDAO.setFullName(cursor.getString(1));
+                loginPartDAO.setRegionName(cursor.getString(2));
+            }
+        }
+        return loginPartDAO;
     }
 }
